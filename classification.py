eb5fc0bc5b47248f8eaaba05565b3eb3e6281693
Varun Shah <varunshah1801@outlook.com>
2026-06-07 20:35:18 +0100

    Add z2 calculation

--- classification.py
+++ classification.py
@@ -24,3 +24,45 @@ def get_weyl_chirality(hamiltonian_func, r, r_, parameters, kpoint, radius=0.005
                                 min_neighbour_dist=5.0E-4)
     return z2pack.invariant.chern(result)
 
+
+def get_x0_data(system):
+    logging.getLogger("z2pack").setLevel(logging.WARNING)
+    result = z2pack.surface.run(system=system, min_neighbour_dist=5.0E-4,
+                                surface=lambda t1, t2: [0, t1/2, t2])
+    return result
+
+
+def get_x1_data(system):
+    logging.getLogger("z2pack").setLevel(logging.WARNING)
+    result = z2pack.surface.run(system=system, min_neighbour_dist=5.0E-4,
+                                surface=lambda t1, t2: [0.5, t1/2, t2])
+    return result
+
+
+def get_y1_data(system):
+    logging.getLogger("z2pack").setLevel(logging.WARNING)
+    result = z2pack.surface.run(system=system, min_neighbour_dist=5.0E-4,
+                                surface=lambda t1, t2: [t1/2, 0.5, t2])
+    return result
+
+
+def get_z1_data(system):
+    logging.getLogger("z2pack").setLevel(logging.WARNING)
+    result = z2pack.surface.run(system=system, min_neighbour_dist=5.0E-4,
+                                surface=lambda t1, t2: [t1/2, t2, 0.5])
+    return result
+
+
+def get_z2_indices(hamiltonian_func, r, r_, parameters):
+    z2pack_sys = initialise_z2pack_system(hamiltonian_func, r, r_, parameters)
+    x0 = z2pack.invariant.z2(get_x0_data(z2pack_sys))
+    x1 = z2pack.invariant.z2(get_x1_data(z2pack_sys))
+    y1 = z2pack.invariant.z2(get_x2_data(z2pack_sys))
+    z1 = z2pack.invariant.z2(get_x3_data(z2pack_sys))
+
+    v0 = int((x0 + x1) % 2)
+    v1 = int(x1)
+    v2 = int(y1)
+    v3 = int(z1)
+
+    return [v0, v1, v2, v3]
